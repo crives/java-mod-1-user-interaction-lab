a8ac85d45bf071c0b8b9fd3233f46e311054a724
Caroline Rives <car.rives@gmail.com>
2022-06-23 15:16:10 -0400

    Updated code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,22 +3,27 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        try(Scanner inputScanner = new Scanner(System.in);) {
-            while(true) {
-                System.out.println("Please enter your age (int):");
+        boolean repeat = true;
+        Scanner inputScanner = new Scanner(System.in);
+        while(repeat) {
+            System.out.println("Please enter your age (int):");
+            try {
                 int userAge = inputScanner.nextInt();
-        
                 if (userAge < 18 && userAge >= 10) {
                     System.out.println("Hello! You are not yet of legal age at the age of " + userAge + "!");
+                    repeat = false;
                 } else if (userAge >= 18) {
                     System.out.println("Hello! You are of legal age at the age of " + userAge + "!");
+                    repeat = false;
                 } else if (userAge < 10) {
                     System.out.println("Hello! Wow, you are getting so big!");
-                }              
+                    repeat = false;
+                }
+            } catch (Exception e) {
+                String str = inputScanner.nextLine();
+                System.out.println("Please enter a valid age (int)!");
+                repeat = true;
             }
-        }  catch (Exception e) {
-            System.out.println("Please enter a valid age (int)!");
         }
     }
 }
-
