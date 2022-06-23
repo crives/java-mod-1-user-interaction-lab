7c889289f0abbfd74eb0f0aefd9c9fb204d9ffab
Caroline Rives <car.rives@gmail.com>
2022-06-23 13:45:41 -0400

    Draft

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // your code here
+        System.out.println("Please enter your age (int):");
+        try(Scanner inputScanner = new Scanner(System.in)) {
+            int userAge = inputScanner.nextInt();
+
+            if (userAge < 18 && userAge >= 10) {
+                System.out.println("Hello! You are not yet of legal age at the age of " + userAge + " !");
+            } else if (userAge >= 18) {
+                System.out.println("Hello! You are of legal age at the age of " + userAge + " !");
+            } else if (userAge < 10) {
+                System.out.println("Hello! Wow, you are getting so big!");
+            }        
+        } catch (Exception e) {
+            System.out.println("Please enter an integer value!");
+        }
     }
 }
+
